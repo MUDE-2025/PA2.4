2cd8cdeebc611224dbf1f2327901983dba05b1a5
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2025-08-22 12:24:22 +0200

    Update test_notebook.py

--- test_notebook.py
+++ test_notebook.py
@@ -6,7 +6,7 @@ def test_values():
         average_monthly_coins = tb.value('float(average_monthly_coins)')
         assert np.isclose(average_monthly_coins, 23.3, atol=5e-2)
 
-        average_monthly_coins_per_year = tb.value('list(average_monthly_coins_per_year)')
+        average_monthly_coins_per_year = tb.value('[float(x) for x in average_monthly_coins_per_year]')
         expected_per_month = np.array([23.8, 22.0, 24.2])
         assert np.allclose(average_monthly_coins_per_year, expected_per_month, atol=5e-2)
 
