d0237561b4de42233bc649fe4215deb093b26d64
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2025-08-22 11:54:54 +0200

    fix test?

--- test_notebook.py
+++ test_notebook.py
@@ -3,21 +3,21 @@ import numpy as np
 
 def test_values():
     with testbook('1_axis.ipynb', execute=True) as tb:
-        average_monthly_coins = tb.ref('average_monthly_coins')
+        average_monthly_coins = tb.value('float(average_monthly_coins)')
         assert np.isclose(average_monthly_coins, 23.3, atol=1e-1)
 
-        average_monthly_coins_per_month = tb.ref('average_monthly_coins_per_month')
+        average_monthly_coins_per_year = tb.value('list(average_monthly_coins_per_year)')
         expected_per_month = np.array([23.8, 22.0, 24.2])
-        assert np.allclose(average_monthly_coins_per_month, expected_per_month, atol=1e-1)
+        assert np.allclose(average_monthly_coins_per_year, expected_per_month, atol=1e-1)
 
-        average_coins_september = tb.ref('average_coins_september')
+        average_coins_september = tb.value('float(average_coins_september)')
         assert np.isclose(average_coins_september, 46.7, atol=1e-1)
 
-        average_coins_january = tb.ref('average_coins_january')
+        average_coins_january = tb.value('float(average_coins_january)')
         assert np.isclose(average_coins_january, 23.7, atol=1e-1)
 
-        max_coins_month = tb.ref('max_coins_month')
+        max_coins_month = tb.value('float(max_coins_month)')
         assert np.isclose(max_coins_month, 53.0, atol=1e-1)
 
-        max_coins_year = tb.ref('max_coins_year')
+        max_coins_year = tb.value('float(max_coins_year)')
         assert np.isclose(max_coins_year, 290.0, atol=1e-1)
