ddb3ef84de98470177dc04fc1e429061acfde7a6
GitHub Action <action@github.com>
2026-04-13 14:43:14 +0000

    Remove requirements and workflow file from solution branch

--- my_test.py
+++ my_test.py
@@ -1,7 +1,7 @@
 from max_even_squared import max_even_squared   #import the function to be tested
 
-def test_regular_case():                                
-    assert max_even_squared([1, 2, 3, 4]) == 16            
+def test_regular_case():
+    assert max_even_squared([1, 2, 3, 4]) == 16
 
 def test_no_even_numbers():
     try:
@@ -10,12 +10,23 @@ def test_no_even_numbers():
     except ValueError as e:
         assert "no even numbers found" in str(e)
 
-# complete the following test methods
 def test_not_a_list():
-    ### YOUR CODE HERE ###
+    try:
+        max_even_squared("apple")
+        assert False, "Expected an exception but none was raised"
+    except TypeError as e:
+        assert "not a list" in str(e)
 
 def test_non_integer_in_list():
-    ### YOUR CODE HERE ###
+    try:
+        max_even_squared([1, 2, "apple"])
+        assert False, "Expected an exception but none was raised"
+    except TypeError as e:
+        assert "only integers are allowed" in str(e)
 
 def test_empty_list():
-    ### YOUR CODE HERE ###
+    try:
+        max_even_squared([])
+        assert False, "Expected an exception but none was raised"
+    except ValueError as e:
+        assert "no even numbers found" in str(e)
